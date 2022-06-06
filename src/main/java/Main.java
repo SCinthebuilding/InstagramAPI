fc30eabfb3372bedc5f813b69d5c5be8a7f0e4ad
Sepid89 <s.jamshididana>
2022-06-06 12:15:55 +0200

    added function get artistName from userInput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,47 +16,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String clientId, clientSecret;
+    public static void main(String[] args) throws IOException, ParseException, SpotifyWebApiException {
+        String clientId, clientSecret,artistName;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("=================================================================");
         System.out.print("Please enter your ClientId : ");
         clientId = scan.nextLine();
         System.out.print("Please enter your ClientSecret : ");
-        clientSecret= scan.nextLine();
+        clientSecret = scan.nextLine();
+        System.out.print("Please enter your Artist's name : ");
+        artistName = scan.nextLine();
         System.out.println("=================================================================");
         Authentication authentication = new Authentication();
-        // Create a request object with the optional parameter "market"
-        GetArtistsTopTracksRequest request = authentication.authenticate(clientId, clientSecret);
-
-        try {
-            // Execute the request synchronous
-            final Track[] tracks = request.execute();
-
-            for (Track track : tracks) {
-                System.out.println(track.getName());
-            }
-
-        } catch (Exception e) {
-            System.out.println("Something went wrong!\n" + e.getMessage());
-        }
-
-        // get number of Albums by artist
-        final GetArtistsAlbumsRequest getArtistsAlbumsRequest = spotifyApi.getArtistsAlbums("2DaxqgrOhkeH0fpeiQq2f4")
-//          .album_type("album")
-//          .limit(10)
-//          .offset(0)
-//          .market(CountryCode.SE)
-                .build();
-
-            try {
-                final Paging<AlbumSimplified> albumSimplifiedPaging = getArtistsAlbumsRequest.execute();
-
-                System.out.println("Total: " + albumSimplifiedPaging.getTotal());
-            } catch (IOException | SpotifyWebApiException | ParseException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
-
+        System.out.printf("Artist's Id : %s ",authentication.returnArtistId(clientId,clientSecret,artistName));
     }
+}
--- src/main/java/utils/Authentication.java
+++ src/main/java/utils/Authentication.java
@@ -5,14 +5,20 @@ import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.model_objects.specification.Artist;
+import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import se.michaelthelin.spotify.requests.data.artists.GetArtistsTopTracksRequest;
+import se.michaelthelin.spotify.requests.data.search.simplified.SearchArtistsRequest;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Authentication {
-    public GetArtistsTopTracksRequest authenticate(String clientId, String clientSecret) {
-        SpotifyApi spotifyApi = null;
+    SpotifyApi spotifyApi = null;
+
+    public void buildSpotify(String clientId, String clientSecret) {
         try {
             spotifyApi = new SpotifyApi.Builder()
                     .setClientId(clientId)
@@ -20,7 +26,7 @@ public class Authentication {
                     .build();
             ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
                     .build();
-            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+            ClientCredentials clientCredentials = clientCredentialsRequest.execute();
 
             // Set access token for further "spotifyApi" object usage
             spotifyApi.setAccessToken(clientCredentials.getAccessToken());
@@ -31,7 +37,17 @@ public class Authentication {
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        return  spotifyApi.getArtistsTopTracks(
-                "2DaxqgrOhkeH0fpeiQq2f4", CountryCode.DE).build();
+    }
+
+    public String returnArtistId(String clientId, String clientSecret, String artistName) throws IOException, ParseException, SpotifyWebApiException {
+        buildSpotify(clientId,clientSecret);
+        SearchArtistsRequest searchArtistsRequest = spotifyApi.searchArtists(artistName)
+                .market(CountryCode.SE)
+                .limit(10)
+                .build();
+
+        Paging<Artist> artistPaging = searchArtistsRequest.execute();
+        List<Artist> artists = Arrays.asList(artistPaging.getItems());
+        return !artists.isEmpty() ? artists.get(0).getId() : "Not found";
     }
 }
