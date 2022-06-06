31c6766401521290723378a59d1784c18e089f09
Sepid89 <s.jamshididana>
2022-06-06 12:46:31 +0200

    created submenu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,28 +16,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         menu();
     }
 
-    public static void menu(){
-        String clientId, clientSecret,artistName;
+    public static void menu() {
+        String clientId, clientSecret, artistName;
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("=================================================================");
+        System.out.println("========== Welcome to search Artist's Songs and Albums ==========");
         System.out.print("Please enter your ClientId : ");
         clientId = scan.nextLine();
         System.out.print("Please enter your ClientSecret : ");
         clientSecret = scan.nextLine();
         System.out.print("Please enter your Artist's name : ");
         artistName = scan.nextLine();
-        System.out.println("=================================================================");
+
         Authentication authentication = new Authentication();
-        try{
-            System.out.printf("Artist's Id : %s ",authentication.returnArtistId(clientId,clientSecret,artistName));
-        }
-        catch (Exception ex){
+        try {
+            String artistId = authentication.returnArtistId(clientId, clientSecret, artistName);
+            System.out.printf("Artist's Id : %s \n", artistId);
+            subMenu(artistId);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        }
+        System.out.println("=================================================================");
+    }
+
+    public static void subMenu(String artistId) {
+        int choice;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter 1 to get 10 top tracks of your artist");
+        System.out.println("enter 2 to get albums of your artist");
+        choice = scan.nextInt();
+        //TODO check choice is valid , 1 or 2
+        switch (choice) {
+            case 1:
+                System.out.println(); //TODO get top tracks of artist
+            case 2:
+                System.out.println(); //TODO get albums of artist
         }
     }
 }
